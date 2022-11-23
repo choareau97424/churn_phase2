a63a6c12d7f92b74a11c106b7f011901c381039b
choareau97424 <46956981+choareau97424@users.noreply.github.com>
2022-11-24 00:16:51 +0100

    Add requestPost_Tests file
    File to run tests on the requests.post functions in the different source code

---
+++ requestPost_Tests.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 23:57:50 2022
+
+@author: choareau
+"""
+
+import sys
+sys.path.insert(0, '../../Web/src/')
+import modelAPI 
+
+correctDataFormat = {
+  'gender': 'Male',
+  'SeniorCitizen': 'Yes',
+  'Partner': 'Yes',
+  'Dependents': 'Yes',
+  'tenure': 0,
+  'PhoneService': 'Yes',
+  'MultipleLines': 'Yes',
+  'InternetService': 'No',
+  'OnlineSecurity': 'No internet service',
+  'OnlineBackup': 'No internet service',
+  'DeviceProtection': 'No internet service',
+  'TechSupport': 'No internet service',
+  'StreamingTV': 'No internet service',
+  'StreamingMovies': 'No internet service',
+  'Contract': 'Two year',
+  'PaperlessBilling': 'Yes',
+  'PaymentMethod': 'Credit card (automatic)',
+  'MonthlyCharges': 0,
+  'TotalCharges': 0
+}
+
+listValue = ['Yes', 'No']
+b = modelAPI.get_prediction(correctDataFormat)
+assert b in listValue
