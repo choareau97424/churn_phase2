fd306fb558d0704f70c17abde3228593c3ffb269
choareau97424 <46956981+choareau97424@users.noreply.github.com>
2022-11-24 01:11:51 +0100

    Update requestPost_Tests.py

--- requestPost_Tests.py
+++ requestPost_Tests.py
@@ -4,36 +4,35 @@ Created on Wed Nov 23 23:57:50 2022
 
 @author: choareau
 """
-import pytest
-import sys
-sys.path.insert(0, 'Backend/Model/src/')
-import modelAPI 
+def test_predict():
+  
+  import sys
+  sys.path.insert(0, 'Backend/Model/src/')
+  import modelAPI 
 
-@pytest.mark.run_these_please
+  correctDataFormat = {
+    'gender': 'Male',
+    'SeniorCitizen': 'Yes',
+    'Partner': 'Yes',
+    'Dependents': 'Yes',
+    'tenure': 0,
+    'PhoneService': 'Yes',
+    'MultipleLines': 'Yes',
+    'InternetService': 'No',
+    'OnlineSecurity': 'No internet service',
+    'OnlineBackup': 'No internet service',
+    'DeviceProtection': 'No internet service',
+    'TechSupport': 'No internet service',
+    'StreamingTV': 'No internet service',
+    'StreamingMovies': 'No internet service',
+    'Contract': 'Two year',
+    'PaperlessBilling': 'Yes',
+    'PaymentMethod': 'Credit card (automatic)',
+    'MonthlyCharges': 0,
+    'TotalCharges': 0
+  }
 
-correctDataFormat = {
-  'gender': 'Male',
-  'SeniorCitizen': 'Yes',
-  'Partner': 'Yes',
-  'Dependents': 'Yes',
-  'tenure': 0,
-  'PhoneService': 'Yes',
-  'MultipleLines': 'Yes',
-  'InternetService': 'No',
-  'OnlineSecurity': 'No internet service',
-  'OnlineBackup': 'No internet service',
-  'DeviceProtection': 'No internet service',
-  'TechSupport': 'No internet service',
-  'StreamingTV': 'No internet service',
-  'StreamingMovies': 'No internet service',
-  'Contract': 'Two year',
-  'PaperlessBilling': 'Yes',
-  'PaymentMethod': 'Credit card (automatic)',
-  'MonthlyCharges': 0,
-  'TotalCharges': 0
-}
+  listValue = ['Yes', 'No']
 
-listValue = ['Yes', 'No']
-
-b = modelAPI.get_prediction(correctDataFormat)
-assert b in listValue
+  b = modelAPI.get_prediction(correctDataFormat)
+  assert b in listValue
