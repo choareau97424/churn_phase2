f91605f4ffd38f7415ee007003ec906e580c12a7
choareau97424 <46956981+choareau97424@users.noreply.github.com>
2022-11-24 21:40:19 +0100

    Update requestPost_Tests.py

--- TestScripts/requestPost_Tests.py
+++ TestScripts/requestPost_Tests.py
@@ -38,38 +38,38 @@ def testModelPrediction():
   modelResponse = modelAPI.get_prediction(correctDataFormat)
   assert modelResponse in listValue
   
-def testWebToModelConnection():
-  
-  import sys
-  sys.path.insert(0, 'Backend/Model/src/')
-  sys.path.insert(0, 'Backend/Web/src/')
-  import modelAPI
-  import webAPI
-
-  correctDataFormat = {
-    'gender': 'Male',
-    'SeniorCitizen': 'Yes',
-    'Partner': 'Yes',
-    'Dependents': 'Yes',
-    'tenure': 0,
-    'PhoneService': 'Yes',
-    'MultipleLines': 'Yes',
-    'InternetService': 'No',
-    'OnlineSecurity': 'No internet service',
-    'OnlineBackup': 'No internet service',
-    'DeviceProtection': 'No internet service',
-    'TechSupport': 'No internet service',
-    'StreamingTV': 'No internet service',
-    'StreamingMovies': 'No internet service',
-    'Contract': 'Two year',
-    'PaperlessBilling': 'Yes',
-    'PaymentMethod': 'Credit card (automatic)',
-    'MonthlyCharges': 0,
-    'TotalCharges': 0
-  }
-
-  listValue = ['Yes', 'No']
-  
-  # Check if Web API returns expected value
-  webResponse = webAPI.get_prediction(correctDataFormat)
-  assert webResponse in listValue
+#def testWebToModelConnection():
+#  
+#  import sys
+#  sys.path.insert(0, 'Backend/Model/src/')
+#  sys.path.insert(0, 'Backend/Web/src/')
+#  import modelAPI
+#  import webAPI
+#
+#  correctDataFormat = {
+#    'gender': 'Male',
+#    'SeniorCitizen': 'Yes',
+#    'Partner': 'Yes',
+#    'Dependents': 'Yes',
+#    'tenure': 0,
+#    'PhoneService': 'Yes',
+#    'MultipleLines': 'Yes',
+#    'InternetService': 'No',
+#    'OnlineSecurity': 'No internet service',
+#    'OnlineBackup': 'No internet service',
+#    'DeviceProtection': 'No internet service',
+#    'TechSupport': 'No internet service',
+#    'StreamingTV': 'No internet service',
+#    'StreamingMovies': 'No internet service',
+#    'Contract': 'Two year',
+#    'PaperlessBilling': 'Yes',
+#    'PaymentMethod': 'Credit card (automatic)',
+#    'MonthlyCharges': 0,
+#    'TotalCharges': 0
+#  }
+#
+#  listValue = ['Yes', 'No']
+#  
+#  # Check if Web API returns expected value
+#  webResponse = webAPI.get_prediction(correctDataFormat)
+#  assert webResponse in listValue
