649de3005de9ce4d4ff9020cdaeba087ea5a5e97
choareau97424 <46956981+choareau97424@users.noreply.github.com>
2022-11-24 23:58:29 +0100

    Delete Backend directory

--- Backend/setup_backend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd Model/
-docker build --tag model-api-docker .
-cd ../Web/
-docker build --tag web-api-docker .
-cd ../Data/
-docker build --tag data-api-docker .
-cd ..
-docker-compose up -d 
