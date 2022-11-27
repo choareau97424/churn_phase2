0465f6fa92a8a83dfef2e1df2d88726eb2fa44c3
choareau97424 <46956981+choareau97424@users.noreply.github.com>
2022-11-27 22:11:23 +0100

    Update checkPredictionFunction.py

--- TestScripts/checkPredictionFunction.py
+++ TestScripts/checkPredictionFunction.py
@@ -8,9 +8,11 @@ def testModelPrediction():
   
   import sys
   sys.path.insert(0, 'Backend/Model/src/')
+  sys.path.insert(0, 'Backend/Data/src/')
   import modelAPI 
+  import dataAPI 
 
-  correctDataFormat = {
+  correctDataFormatModel = {
     'gender': 'Male',
     'SeniorCitizen': 'Yes',
     'Partner': 'Yes',
@@ -31,20 +33,49 @@ def testModelPrediction():
     'MonthlyCharges': 0,
     'TotalCharges': 0
   }
+  
+  correctDataFormatData = {
+    'customerID': '1234-ABCDE'
+    'gender': 'Male',
+    'SeniorCitizen': 'Yes',
+    'Partner': 'Yes',
+    'Dependents': 'Yes',
+    'tenure': 0,
+    'PhoneService': 'Yes',
+    'MultipleLines': 'Yes',
+    'InternetService': 'No',
+    'OnlineSecurity': 'No internet service',
+    'OnlineBackup': 'No internet service',
+    'DeviceProtection': 'No internet service',
+    'TechSupport': 'No internet service',
+    'StreamingTV': 'No internet service',
+    'StreamingMovies': 'No internet service',
+    'Contract': 'Two year',
+    'PaperlessBilling': 'Yes',
+    'PaymentMethod': 'Credit card (automatic)',
+    'MonthlyCharges': 0,
+    'TotalCharges': 0,
+    'Churn': 'Yes',
+    'ChurnPrediction': 'Yes'
+  }
 
   listValue = ['Yes', 'No']
   
   # Check if Model API returns expected value
-  modelResponse = modelAPI.get_prediction(correctDataFormat)
+  modelResponse = modelAPI.get_prediction(correctDataFormatModel)
   assert modelResponse in listValue
+  
+  # Check if Data API returns expected value
+  dataResponse = dataAPI.save_data(correctDataFormatModelData)
+  assert dataResponse == 'New entry saved'
   
 #def testWebToModelConnection():
 #  
 #  import sys
 #  sys.path.insert(0, 'Backend/Model/src/')
-#  sys.path.insert(0, 'Backend/Web/src/')
+#  sys.path.insert(0, 'Backend/Data/src/')
 #  import modelAPI
-#  import webAPI
+#  import dataAPI
 #
 #  correctDataFormat = {
 #    'gender': 'Male',
